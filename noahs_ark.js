8506b03857c4596451e4b5fb6d75b754247b3bc3
Maxime Salomon <maxime@croissant.io>
2020-02-17 11:12:09 +0100

    add UML diagram + sail.js

--- noahs_ark.js
+++ noahs_ark.js
@@ -13,5 +13,3 @@ const noahs_ark = new Ark();
 
 noahs_ark.id = 1;
 noahs_ark.name = "Noah's Ark";
-
-console.log(noahs_ark.id + " " + noahs_ark.name);
--- sail.js
+++ sail.js
@@ -1,13 +1,18 @@
 const uuidv4 = require("uuid/v4");
 
 class Sail {
-  constructor(id) {
+  constructor(id, height, width, lenght, color) {
     this.id = id;
+    this.height = height; // in mm
+    this.width = width; // in mm
+    this.lenght = lenght; // in mm
+    this.color = color;
   }
 }
 
 const sail = new Sail();
-sail.id = uidv4();
-
-
-// TODO INTERFACE
+sail.id = uuidv4();
+sail.height = 20000;
+sail.width = 150000;
+sail.lenght = 250000;
+sail.color = "blue";
